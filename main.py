6e8f5074680176e1c06b5a439cdd7e6fa041ed30
RussianAssault <125305076+RussianAssaulter@users.noreply.github.com>
2023-05-17 13:07:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ import requests
 import os
 import time
 
+def init(): 
+ os.system("cls & title [Webhook Spammer] - By: Russian")
+ time.sleep(0.1)
+ main()
+ 
+
+        
 
 
-webhook = input("Webhook: \n")
-message = input(("Message: \n"))
 
-os.system("cls & title [Webhook Spammer] - By: Russian")
 
-while True:
-     time.sleep(1)
+
+def main():
+ os.system("cls & title [Webhook Spammer] - By: Russian")
+ webhook = input("Webhook: \n")
+ message = input(("Message: \n"))
+ while True:
      r = requests.post(webhook, json={"content": message})
      if r.status_code == 204:
                print("Sent message successfully")
+     elif r.status_code == 429:
+        print("Webhook Ratelimited Please Switch Webhooks")
+        time.sleep(2)
+        main()
+
+
+init()
